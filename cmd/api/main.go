787210e5b284492f7f9c2394681723fd3a5c819d
Matthew Baldeosingh <baldeosinghsm@gmail.com>
2026-03-24 10:41:52 -0400

    feat: Set up chi router to handle http requests

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	r := chi.NewRouter() // Set up router
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+
 	fmt.Println("upskill-api starting...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r) // Pass chi as the router for http requests
 }

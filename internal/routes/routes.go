52f5752568941c65ba8da8953eab16732e022f52
Matthew Baldeosingh <baldeosinghsm@gmail.com>
2026-04-27 13:35:12 -0400

    feat: Add http routes to courses

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/baldeosinghm/upskill/internal/courses"
 	"github.com/baldeosinghm/upskill/internal/users"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -21,13 +22,23 @@ func RegisterRoutes(db *pgxpool.Pool) *chi.Mux {
 		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
 	})
 
-	// Users domain instances
 	uRepo := users.NewRepository(db)
 	uService := users.NewService(uRepo)
 	uHandler := users.NewHandler(uService)
 
+	cRepo := courses.NewRepository(db)
+	cService := courses.NewService(cRepo, uService)
+	cHandler := courses.NewHandler(cService)
+
 	// Route requests
 	r.Post("/users", uHandler.Create)
 	r.Post("/login", uHandler.Login)
+
+	r.Route("/courses", func(r chi.Router) {
+		r.Post("/", cHandler.Create)
+		r.Get("/{id}", cHandler.GetByID)
+		r.Get("/", cHandler.List)
+	})
+
 	return r
 }
